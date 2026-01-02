c044cee80834a358d5daf348ee1cc43efad85fe2
Tobias Faßbender <tobias-fassbender@gmx.de>
2026-01-02 12:56:08 +0100

    handling of obelisk animations and magic fire in the battle with the chaos wizard

--- core/src/net/jfabricationgames/gdx/event/coded/CastleOfTheChaosWizardBossFightStage3EventHandler.java
+++ core/src/net/jfabricationgames/gdx/event/coded/CastleOfTheChaosWizardBossFightStage3EventHandler.java
@@ -6,6 +6,11 @@ import net.jfabricationgames.gdx.event.EventType;
 
 public class CastleOfTheChaosWizardBossFightStage3EventHandler extends CodedEventHandler {
 	
+	private boolean obeliskDestroyedTopLeft = false;
+	private boolean obeliskDestroyedTopRight = false;
+	private boolean obeliskDestroyedBottomLeft = false;
+	private boolean obeliskDestroyedBottomRight = false;
+	
 	@Override
 	public void handleEvent(EventConfig event) {
 		// TODO delete after tests
@@ -23,15 +28,19 @@ public class CastleOfTheChaosWizardBossFightStage3EventHandler extends CodedEven
 			String animationObjectIdToTurnOff = null;
 			if ("loa2_l5_castle_of_the_chaos_wizard__spire__magic_obelisk_top_left".equals(event.stringValue)) {
 				animationObjectIdToTurnOff = "loa2_l5_castle_of_the_chaos_wizard__spire__magic_energy_line_top_left";
+				obeliskDestroyedTopLeft = true;
 			}
 			else if ("loa2_l5_castle_of_the_chaos_wizard__spire__magic_obelisk_top_right".equals(event.stringValue)) {
 				animationObjectIdToTurnOff = "loa2_l5_castle_of_the_chaos_wizard__spire__magic_energy_line_top_right";
+				obeliskDestroyedTopRight = true;
 			}
 			else if ("loa2_l5_castle_of_the_chaos_wizard__spire__magic_obelisk_bottom_left".equals(event.stringValue)) {
 				animationObjectIdToTurnOff = "loa2_l5_castle_of_the_chaos_wizard__spire__magic_energy_line_bottom_left";
+				obeliskDestroyedBottomLeft = true;
 			}
 			else if ("loa2_l5_castle_of_the_chaos_wizard__spire__magic_obelisk_bottom_right".equals(event.stringValue)) {
 				animationObjectIdToTurnOff = "loa2_l5_castle_of_the_chaos_wizard__spire__magic_energy_line_bottom_right";
+				obeliskDestroyedBottomRight = true;
 			}
 			
 			if (animationObjectIdToTurnOff != null) {
@@ -40,6 +49,13 @@ public class CastleOfTheChaosWizardBossFightStage3EventHandler extends CodedEven
 						.setEventType(EventType.DISPLAY_ANIMATION_GAME_OBJECT) //
 						.setStringValue(animationObjectIdToTurnOff) //
 						.setBooleanValue(false));
+			}
+			
+			if (obeliskDestroyedTopLeft && obeliskDestroyedTopRight && obeliskDestroyedBottomLeft && obeliskDestroyedBottomRight) {
+				// all obelisks destroyed -> remove the magic shield around the Chaos Wizard
+				EventHandler.getInstance().fireEvent(new EventConfig() //
+						.setEventType(EventType.TRAVERSABLE_OBJECT_CHANGE_BODY_TO_SENSOR) //
+						.setIntValue(3));
 			}
 		}
 	}
