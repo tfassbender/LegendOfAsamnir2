5a5beab79f9e31b5e9337a93a147e519cdcea2fe
Tobias Faßbender <tobias-fassbender@gmx.de>
2026-01-04 12:34:46 +0100

    bugfixes: magic flames don't block projectiles; bombs don't deal damage sometimes

--- core/src/net/jfabricationgames/gdx/projectile/Explosion.java
+++ core/src/net/jfabricationgames/gdx/projectile/Explosion.java
@@ -1,7 +1,7 @@
 package net.jfabricationgames.gdx.projectile;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Contact;
 
 import net.jfabricationgames.gdx.animation.AnimationDirector;
 import net.jfabricationgames.gdx.attack.AttackConfig;
@@ -9,6 +9,9 @@ import net.jfabricationgames.gdx.physics.PhysicsBodyCreator.PhysicsBodyPropertie
 import net.jfabricationgames.gdx.physics.PhysicsBodyCreator.PhysicsBodyShape;
 
 public class Explosion extends Projectile {
+	
+	private boolean processedContact = false;
+	private boolean explosionDamageDealt = false;
 	
 	public Explosion(ProjectileTypeConfig typeConfig, AttackConfig attackConfig, AnimationDirector<TextureRegion> animation, ProjectileMap gameMap) {
 		super(typeConfig, attackConfig, animation, gameMap);
@@ -21,17 +24,22 @@ public class Explosion extends Projectile {
 	
 	@Override
 	public void update(float delta) {
+		if (processedContact) {
+			// explosion damage is dealt multiple times (multiple targets) but only in one update cycle
+			explosionDamageDealt = true;
+		}
+		
 		super.update(delta);
-		removeExplosionSensor();
 	}
 	
-	private void removeExplosionSensor() {
-		if (hasBody()) {
-			for (Fixture fixture : body.getFixtureList()) {
-				if (fixture.isSensor()) {
-					body.destroyFixture(fixture);
-				}
-			}
+	@Override
+	protected void processContact(Object contactUserData, Contact contact) {
+		if (explosionDamageDealt) {
+			// prevent dealing damage multiple times in multiple update cycles (but allow multiple targets in one update cycle)
+			return;
 		}
+		
+		super.processContact(contactUserData, contact);
+		processedContact = true;
 	}
 }
